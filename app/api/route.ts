0188a9ad9a29c1d93c5b88234620bbb6e66ff925
James Acklin <jamesacklin@gmail.com>
2023-08-27 13:40:35 -0400

    api: better design, source data

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,5 +1,5 @@
-import path from "path";
 import fs from "fs";
+import path from "path";
 import { v4 as uuidv4 } from "uuid";
 import seedrandom from "seedrandom";
 
@@ -10,10 +10,15 @@ interface Actor {
 }
 
 const filePath = path.join(process.cwd(), "data.json");
-const fileData: string = fs.readFileSync(filePath, "utf8");
-const actors: Actor[] = JSON.parse(fileData);
+let actors: Actor[] = [];
+try {
+  const fileData = fs.readFileSync(filePath, "utf8");
+  actors = JSON.parse(fileData);
+} catch (error) {
+  console.log(error);
+}
 
-const shuffle = (array: string[], rng: () => number): string[] => {
+const shuffle = (array: Array<string>, rng: () => number) => {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(rng() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
@@ -21,63 +26,75 @@ const shuffle = (array: string[], rng: () => number): string[] => {
   return array;
 };
 
-const generateRandomMatrix = () => {
-  const uuid: string = uuidv4();
-  const rng: () => number = seedrandom(uuid);
-
-  let intersections: { [key: string]: string[] } = {};
-
-  actors.forEach((actor: Actor) => {
-    actor.directors.forEach((rowDirector: string) => {
-      actor.directors.forEach((colDirector: string) => {
-        if (rowDirector === colDirector) return;
-
-        const key: string = `${rowDirector}-${colDirector}`;
-        if (!intersections[key]) intersections[key] = [];
-
-        intersections[key].push(actor.name);
-      });
-    });
-  });
-
-  // Get unique directors and shuffle them using the seeded RNG
-  let uniqueDirectors: string[] = Array.from(
-    new Set(actors.flatMap((actor: Actor) => actor.directors))
-  );
-  shuffle(uniqueDirectors, rng);
-
-  // Select 3 unique directors for rows and columns
-  let rowDirectors: string[] = uniqueDirectors.slice(0, 3);
-  let colDirectors: string[] = uniqueDirectors.slice(3, 6);
-
-  let matrix: string[][][] = [];
-
-  // Generate the 3x3 matrix
-  rowDirectors.forEach((rowDirector: string) => {
-    let row: string[][] = [];
-    colDirectors.forEach((colDirector: string) => {
-      const key: string = `${rowDirector}-${colDirector}`;
-      row.push(intersections[key] || []);
-    });
-    matrix.push(row);
-  });
-
-  return {
-    uuid,
-    rowDirectors,
-    colDirectors,
-    matrix,
-  };
-};
-
 export async function GET() {
-  const { uuid, rowDirectors, colDirectors, matrix } = generateRandomMatrix();
+  const uuid = uuidv4();
+  const rng = seedrandom(uuid);
+
+  const directorMap: Record<string, Set<string>> = {};
+
+  actors.forEach((actor) => {
+    actor.directors.forEach((director) => {
+      if (!directorMap[director]) {
+        directorMap[director] = new Set();
+      }
+      directorMap[director].add(actor.name);
+    });
+  });
+
+  const eligibleDirectors = Object.keys(directorMap).filter(
+    (director) => Array.from(directorMap[director]).length >= 3
+  );
+
+  if (eligibleDirectors.length < 6) {
+    return new Response(
+      JSON.stringify({ message: "Not enough eligible directors" }),
+      {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+
+  shuffle(eligibleDirectors, rng);
+
+  const rowDirectors = eligibleDirectors.slice(0, 3);
+  const colDirectors = eligibleDirectors.slice(3, 6);
+
+  const matrix: string[][][] = [];
+
+  for (let rowDir of rowDirectors) {
+    const row: string[][] = [];
+    for (let colDir of colDirectors) {
+      const intersection = Array.from(directorMap[rowDir]).filter((actor) =>
+        directorMap[colDir].has(actor)
+      );
+      if (intersection.length < 3) {
+        return new Response(
+          JSON.stringify({
+            message: "Not enough actor/director intersections for a 3x3 matrix",
+          }),
+          {
+            status: 200,
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+      }
+      shuffle(intersection, rng);
+      row.push(intersection.slice(0, 3)); // Slicing to ensure we get only 3 actors
+    }
+    matrix.push(row);
+  }
 
   return new Response(
     JSON.stringify({
-      directors: { rows: [...rowDirectors], cols: [...colDirectors] },
+      directors: { rows: rowDirectors, cols: colDirectors },
       actors: matrix,
       uuid: uuid,
+      message: "success",
     }),
     {
       status: 200,
