d7830f6849620d0917ca6a9b2e62125bce700858
James Acklin <jamesacklin@gmail.com>
2023-09-01 19:23:12 -0400

    front-end: add cmdk for input

--- app/components/InputField.tsx
+++ app/components/InputField.tsx
@@ -1,61 +1,66 @@
-import Downshift from "downshift";
+import { useState, useEffect } from "react";
+import cn from "classnames";
+import { Command } from "cmdk";
+import _ from "lodash";
 
 export default function InputField(props: any) {
   const { items, id, name } = props;
+  const [value, setValue] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const handleBlur = (e: any, value: string) => {
+    if (e.relatedTarget && e.relatedTarget.classList.contains("listbox")) {
+      setValue(value);
+      setTimeout(() => {
+        setOpen(false);
+      }, 100);
+    } else {
+      setOpen(false);
+    }
+  };
+
+  const handleSet = (value: string) => {
+    const match = items.findIndex(
+      (item: string) => value.toLowerCase() === item[0].toLowerCase()
+    );
+    console.log(match);
+  };
 
   return (
-    <>
-      <Downshift itemToString={(item) => (item ? item.value : "")}>
-        {({
-          getInputProps,
-          getItemProps,
-          getLabelProps,
-          getMenuProps,
-          isOpen,
-          inputValue,
-          getRootProps,
-        }) => (
-          <div className="relative">
-            <label {...getLabelProps()} className="sr-only">
-              Enter an actor
-            </label>
-
-            <div
-              className="h-10 w-48 flex justify-between items-center"
-              {...getRootProps({}, { suppressRefError: true })}
-            >
-              <input
-                className="w-full px-3 py-2 m-0 bg-gray-100 border-2 rounded-md focus:bg-white focus:outline-none focus:border-blue-500"
-                {...getInputProps({ id: id, name: name })}
-              />
-            </div>
-            <ul
-              className="bg-white z-50 absolute w-full shadow-lg mt-1 rounded-md overflow-clip"
-              {...getMenuProps()}
-            >
-              {isOpen
-                ? items
-                    .filter(
-                      (item: any) =>
-                        !inputValue || item.value.includes(inputValue)
-                    )
-                    .map((item: any, index: number) => (
-                      <li
-                        className="px-3 py-2 text-sm cursor-pointer hover:bg-gray-100"
-                        key={item.value}
-                        {...getItemProps({
-                          index,
-                          item,
-                        })}
-                      >
-                        {item.value}
-                      </li>
-                    ))
-                : null}
-            </ul>
-          </div>
+    <Command label="Select an actor" className="relative">
+      <Command.Input
+        id={id}
+        name={name}
+        value={value}
+        onValueChange={(v) => setValue(v)}
+        onFocus={() => setOpen(true)}
+        onBlur={(e) => handleBlur(e, value)}
+        placeholder="Type or select an actor"
+        className="text-base border-2 border-gray-200 rounded-md w-full py-1.5 px-3 text-gray-900 focus:outline-none focus:bg-white focus:rounded-b-none focus:border-gray-600"
+      />
+      <Command.List
+        tabIndex={-1}
+        className={cn(
+          "bg-white rounded-b-md border-2 border-gray-600 overflow-clip drop-shadow-lg absolute w-full mt-[-2px] z-50 content listbox",
+          !open && "hidden"
         )}
-      </Downshift>
-    </>
+      >
+        <Command.Empty className="text-gray-900 text-sm px-3 py-2">
+          No one here by that name.
+        </Command.Empty>
+        {items.map((item: any) => (
+          <Command.Item
+            value={item}
+            className="text-sm px-3 py-2 text-gray-900 hover:bg-gray-200 cursor-pointer"
+            onSelect={(v) => {
+              handleSet(v);
+            }}
+            key={item}
+          >
+            {item}
+          </Command.Item>
+        ))}
+      </Command.List>
+    </Command>
   );
 }
